550d2c91b6668e279ccc1dcd201da1820275f837
daan23411 <46263539+daan23411@users.noreply.github.com>
2020-06-19 13:00:15 +0200

    t

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,5 +1,5 @@
 const {prefix}= require('../../config.json')
 module.exports=bot=>{
-    bot.user.setActivity(`${prefix}help | Amsterdam Zuid`)
+    bot.user.setActivity(`${prefix}help | ${message.guild.name}`)
     console.log(`Hoi! ${bot.user.username} is beschikbaar voor commands!`)
 }
