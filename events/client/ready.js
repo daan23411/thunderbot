17b3fef55bf261a86bd1df7f6039e95e5d6dac75
daan23411 <46263539+daan23411@users.noreply.github.com>
2020-06-19 13:03:46 +0200

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,6 +1,6 @@
 const {prefix}= require('../../config.json')
 const { MessageEmbed } = require('discord.js')
 module.exports=bot=>{
-    bot.user.setActivity(`${prefix}help | ${message.guild.name}`)
+    bot.user.setActivity(`${prefix}help | ThunderNetwork`)
     console.log(`Hoi! ${bot.user.username} is beschikbaar voor commands!`)
 }
