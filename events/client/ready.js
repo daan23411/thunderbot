ef88acb469a5003a19e126d1022d3910ba950013
daan23411 <46263539+daan23411@users.noreply.github.com>
2020-06-19 13:02:44 +0200

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,5 @@
 const {prefix}= require('../../config.json')
+const { MessageEmbed } = require('discord.js')
 module.exports=bot=>{
     bot.user.setActivity(`${prefix}help | ${message.guild.name}`)
     console.log(`Hoi! ${bot.user.username} is beschikbaar voor commands!`)
